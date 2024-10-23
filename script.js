fe130fe2d1a98974397d5f487b548ae7001d8cf8
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-23 15:19:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+	//Add your code here
+	const allPrice= document.querySelectorAll(".price");
+	let sum= 0; 
+	allPrice.forEach(price => {
+		sum+=parseInt(price.innerText);
+	})
+	const total = document.createElement("div");
+	total.innerText= sum;
+	document.body.appendChild(total);
 };
 
 getSumBtn.addEventListener("click", getSum);
